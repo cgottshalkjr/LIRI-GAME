d23fef8fd917e47ba9f7f40c753a256668f6bcb8
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-11-11 23:18:39 -0500

    Fixed minor bugs, Added readme, added images.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # LIRI-GAME
+---
+>Created by **Christopher Gottshalk**
+---
+**LIRI-GAME** is an node based app where everything happens on the command line in terminal. There are multiples node packages being installed in this app. AXIOS, SPOTIFY, MOMENT. 
+
+
+### INSTRUCTIONS 
+---
+1. Open the app in your terminal. 
+2. Make sure to type **node** followed by **liri.js** as your command line arguments.
+3. Next type one of these four commands...
+    * **movie-this** (followed by a movie you want information about)
+    * **concert-this** (followed by a band you would like to see in concert)
+    * **spotify-this-song** (followed by a song you would like to hear a preview of)
+    * **do-what-it-says** (you will type this by itself)
+4. If you don't follow the commands an auto selection will be provided
+
+---
+
+### Pictures of Game in Action!
+---
+![alt text](img/moviethis2.png "movie-this")
+---
+---
+![alt text](img/concertthis.png "concert-this")
+---
+---
+![alt text](img/spot.png "spotify-this-song")
+---
+---
+![alt text](img/dowhat.png "do-what-it-says")
+---
+---
+### TECHNOLOGIES USED
+---
+* Javascript
+* Nodejs
+* Node packages:
+* Node-Spotify-API
+* Axios
+* Moment
+ 
+* Bands in Town
+* OMDB
+* Git
+* GitHub
--- liri.js
+++ liri.js
@@ -6,13 +6,16 @@ var fs = require("fs");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-var inquirer = require("inquirer");
+// var inquirer = require("inquirer");
 
+//setting commands we will use to variables on spots 2 and 3 after node liri.js
 var userCommand = process.argv[2];
 var userRequest = process.argv.slice(3).join(" ");
 
-
+//function containing our switch that will help with our do-what function later
 function runGame(userCommand, userRequest) {
+
+    //switch holding all of the functions for the app
     switch (userCommand) {
 
         case "movie-this":
@@ -34,8 +37,9 @@ function runGame(userCommand, userRequest) {
         default:
             console.log("Enter a valid search command")
 
-    }
+    } //End of switch
 }
+//End of run game function
 
 //Function for handling errors with Rotten Tomatoes score
 function getRotten(tomatoes) {
@@ -56,6 +60,7 @@ function movieFunction(movieTitle) {
 
     axios.get(queryUrl)
         .then(function (response) {
+            console.log("-------------*MOVIE--INFO*-------------")
             console.log("---------------------------------------");
             console.log("Title: " + response.data.Title);
             console.log("---------------------------------------");
@@ -64,19 +69,19 @@ function movieFunction(movieTitle) {
             console.log("||||||Release Year: " + response.data.Year);
             response.data.Ratings.map(getRotten);
             console.log("||||||IMDb: " + response.data.Ratings[0].Value);
-            console.log("||||||MetaCritic: " + response.data.Ratings[2].Value);
             console.log("||||||Plot: " + response.data.Plot);
             console.log("||||||Country: " + response.data.Country);
             console.log("||||||Language: " + response.data.Language)
             console.log("===========================================");
 
-            var logText = "-------------------------------------\n" +
+            var logText = "-------------*MOVIE--INFO*-------------\n" +
+                "-------------------------------------\n" +
                 "Title: " + response.data.Title + "\n" +
                 "-------------------------------------\n" +
                 "Directed By: " + response.data.Director + "\n" +
                 "Starring: " + response.data.Actors + "\n" +
                 "Release Year: " + response.data.Year + "\n" +
-                "||||||IMDb: " + response.data.Ratings[0].Value + "\n" + "||||||MetaCritic: " + response.data.Ratings[2].Value + "\n" + "||||||Plot: " + response.data.Plot + "\n" + "||||||Country: " + response.data.Country + "\n" + "||||||Language: " + response.data.Language + "\n"
+                "||||||IMDb: " + response.data.Ratings[0].Value + "\n" + "||||||Plot: " + response.data.Plot + "\n" + "||||||Country: " + response.data.Country + "\n" + "||||||Language: " + response.data.Language + "\n"
                 + "======================================\n"
 
             fs.appendFile("log.txt", logText, function (err) {
@@ -104,8 +109,6 @@ function movieFunction(movieTitle) {
             }
             console.log(error.config);
         })
-
-
 }
 //End of movieFunction
 
@@ -122,13 +125,13 @@ function concertFunction(artistSearch) {
         .then(function (response) {
             if (response.data.length > 0) {
                 for (var i = 0; i < response.data.length; i++) {
-                    console.log("---------------------------------------");
+                    console.log("------------------*EVENTS*---------------------");
                     console.log("Venue: " + response.data[i].venue.name);
                     console.log("City: " + response.data[i].venue.city + ", " + (response.data[i].venue.region || response.data[i].venue.country));
                     console.log("When: " + moment(response.data[i].datetime).format("LLLL"));
                     console.log("---------------------------------------");
 
-                    var logText = "-------------------------------------\n" +
+                    var logText = "------------------*EVENTS*---------------------\n" +
                         "Venue: " + response.data[i].venue.name + "\n" +
                         "-------------------------------------\n" +
                         "City: " + response.data[i].venue.city + ", " + (response.data[i].venue.region || response.data[i].venue.country) + "\n" +
@@ -150,6 +153,7 @@ function concertFunction(artistSearch) {
 }
 //End of concertFunction
 
+//Creating function to run spotify-this-song
 function spotifyFunction(songTitle) {
 
     if (!songTitle) {
@@ -163,16 +167,18 @@ function spotifyFunction(songTitle) {
 
 
         for (var i = 0; i < response.tracks.items.length; i++) {
-            console.log("-------------------------------------")
+            console.log("****************MUSIC****************");
+            console.log("-------------------------------------");
             console.log("Artist: " + response.tracks.items[i].artists[0].name);
-            console.log("-------------------------------------")
+            console.log("-------------------------------------");
             console.log("Song: " + response.tracks.items[i].name);
             console.log("URL: " + response.tracks.items[i].preview_url || "NO PREVIEW FOR YOU!!!");
             console.log("Album: " + response.tracks.items[i].album.name);
             console.log("======================================");
 
 
-            var logText = "-------------------------------------\n" +
+            var logText = "****************MUSIC****************\n" +
+                "-------------------------------------\n" +
                 "Artist: " + response.tracks.items[i].artists[0].name + "\n" +
                 "-------------------------------------\n" +
                 "Song: " + response.tracks.items[i].name + "\n" +
@@ -187,11 +193,11 @@ function spotifyFunction(songTitle) {
 
             });
         }
-
-
     });
 };
+//End of spotify function
 
+//Creating a function to do-what-it-says
 function doWhatFunction() {
 
     fs.readFile('random.txt', 'utf8', function (err, response) {
@@ -208,6 +214,11 @@ function doWhatFunction() {
 
     });
 }
+//End of do-what function
 
+//function to run game with global command variables passed in as arguments
 runGame(userCommand, userRequest);
+
+
+
 
